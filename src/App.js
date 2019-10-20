4b04fdd220b39d6ef772a15757c064923d194401
Tobias Graf <tobiasgraf87@gmail.com>
2019-10-20 21:19:09 +0200

    fixed prettier in vscode

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from "react"
 
-import {observer} from 'mobx-react'
+import { observer } from "mobx-react"
 
-const Todo = observer(({todo}) => {
-    return (<li
-    key={todo.id}
-    style={{textDecoration : todo.done ? "line-through" : ""}}
-    onClick={() => todo.mark(!todo.done)}>
-    {todo.value}
-  </li>)
+const Todo = observer(({ todo }) => {
+    return (
+        <li
+            key={todo.id}
+            style={{ textDecoration: todo.done ? "line-through" : "" }}
+            onClick={() => todo.mark(!todo.done)}
+        >
+            {todo.value}
+        </li>
+    )
 })
 
-
-const App = observer((props) => {
-  const [value, setValue] = useState("")
-  return (
-    <div className="App">
-    <form  onSubmit={e => {
-      e.preventDefault(); props.todoList.addTodo(value)}}>
-
-    <input type="text" value={value} onChange={(e)=> setValue(e.target.value)} />
-    </form>
-    <ul>
-      {props.todoList.list.map((todo) =>
-        <Todo key={todo.id} todo={todo}/>
-      )}
-    </ul>
-    </div>
-  );
+const App = observer(props => {
+    const [value, setValue] = useState("")
+    return (
+        <div className="App">
+            <form
+                onSubmit={e => {
+                    e.preventDefault()
+                    props.todoList.addTodo(value)
+                }}
+            >
+                <input
+                    type="text"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
+            </form>
+            <ul>
+                {props.todoList.list.map(todo => (
+                    <Todo key={todo.id} todo={todo} />
+                ))}
+            </ul>
+        </div>
+    )
 })
 
-export default App;
+export default App
